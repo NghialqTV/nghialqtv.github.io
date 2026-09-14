9e15eeaffec28a7d480700382d1ef8977901c3c8
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-09-14 22:06:07 +0700

    Update config.js

--- tiktok_ads/config.js
+++ tiktok_ads/config.js
@@ -1,7 +1,7 @@
 window.TIKTOK_AD_LINKS = [
-  "https://vt.tiktok.com/ZS9S9LCuRvkmJ-ghNEH/",
+  "https://s.shopee.vn/6L4RAk9bAl",
   "https://vt.tiktok.com/ZS9S9NSUxhwDH-3Mazt/",
-  "https://vt.tiktok.com/ZS9S9NMrsoDUe-3CFNK/",
+  "https://s.shopee.vn/9AOcY3CCRs",
   "https://vt.tiktok.com/ZS9Bsh29NRboX-rzCEr/",
-  "https://vt.tiktok.com/ZS9S9Lps8C4qw-fmK1y/"
+  "https://s.shopee.vn/6q0hloFGVV"
 ];
